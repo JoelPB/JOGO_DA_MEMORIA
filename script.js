6c30927eca71483fb4bdb70faae3553fb9ba1840
JoelPB <coffee.program.joel@gmail.com>
2021-03-14 11:06:24 -0300

    up to part 6

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ let lockBoard = false;
 
 function flipCard() {
     if (lockBoard) return;
+    if (this === firstCard) return;
+
     this.classList.add('flip')
     if (!hasFlippedCard) {
         hasFlippedCard = true;
@@ -27,15 +29,26 @@ function checkForMatch(){
 }
 
 function desableCards(){
-    firstCard.removeEventListener('click', flipCard)
-    secondCard.removeEventListener('click', flipCard)
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+
+    resetBoard();
 }
 
 function unflipCards(){
+    lockBoard = true;
+
     setTimeout(() => {
         firstCard.classList.remove('flip');
         secondCard.classList.remove('flip');
+
+        resetBoard();
     }, 1500)
+}
+
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
 }
 
 cards.forEach((card) => {
